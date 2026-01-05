8e59cba485dd88bdf11180262e88d8b8b76a0d2c
Ted-Hsu0630 <w42622@gmail.com>
2026-01-05 18:13:04 +0800

    加入GDN/IGDN 移除蒸餾程式

--- compressai/models/tic.py
+++ compressai/models/tic.py
@@ -1,294 +1,10 @@
-# # 檔案名稱: tic_model.py
-# # (您提供的 BatchNorm 版本，已修改 forward 以回傳 'y' 供蒸餾使用)
-#
-# import math
-# import torch
-# import torch.nn as nn
-# import torch.nn.functional as F
-#
-# from compressai.entropy_models import EntropyBottleneck, GaussianConditional
-# from compressai.layers import RSTB_BN, CausalAttentionModule
-# from compressai.ans import BufferedRansEncoder, RansDecoder
-# from timm.models.layers import trunc_normal_
-# from compressai.models.utils import conv, deconv, update_registered_buffers
-#
-# SCALES_MIN = 0.11
-# SCALES_MAX = 256
-# SCALES_LEVELS = 64
-#
-#
-# def get_scale_table(min=SCALES_MIN, max=SCALES_MAX, levels=SCALES_LEVELS):
-#     return torch.exp(torch.linspace(math.log(min), math.log(max), levels))
-#
-#
-# class TIC(nn.Module):
-#     """
-#     使用 BatchNorm 的 TIC 模型。
-#     已修改 forward 函式以回傳中間特徵 'y'，用於知識蒸餾。
-#     """
-#
-#     def __init__(self, N=128, M=192):
-#         super().__init__()
-#         depths = [2, 4, 6, 2, 2]
-#         num_heads = [4, 8, 16, 16, 16]
-#         window_size = 8
-#         mlp_ratio = 4.
-#         qkv_bias = True
-#         qk_scale = None
-#         drop_rate = 0.
-#         attn_drop_rate = 0.
-#         drop_path_rate = 0.2
-#         use_checkpoint = False
-#         self.M = M
-#         self.N = N
-#
-#         dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]
-#
-#         self.g_a0 = conv(3, N, kernel_size=5, stride=2)
-#         self.g_a1 = RSTB_BN(dim=N, input_resolution=(128, 128), depth=depths[0], num_heads=num_heads[0],
-#                             window_size=window_size, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-#                             drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[sum(depths[:0]):sum(depths[:1])],
-#                             use_checkpoint=use_checkpoint)
-#         self.g_a2 = conv(N, N, kernel_size=3, stride=2)
-#         self.g_a3 = RSTB_BN(dim=N, input_resolution=(64, 64), depth=depths[1], num_heads=num_heads[1],
-#                             window_size=window_size, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-#                             drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[sum(depths[:1]):sum(depths[:2])],
-#                             use_checkpoint=use_checkpoint)
-#         self.g_a4 = conv(N, N, kernel_size=3, stride=2)
-#         self.g_a5 = RSTB_BN(dim=N, input_resolution=(32, 32), depth=depths[2], num_heads=num_heads[2],
-#                             window_size=window_size, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-#                             drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[sum(depths[:2]):sum(depths[:3])],
-#                             use_checkpoint=use_checkpoint)
-#         self.g_a6 = conv(N, M, kernel_size=3, stride=2)
-#
-#         self.h_a0 = conv(M, N, kernel_size=3, stride=1)
-#         self.h_a1 = RSTB_BN(dim=N, input_resolution=(16, 16), depth=depths[3], num_heads=num_heads[3],
-#                             window_size=window_size // 2, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-#                             drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[sum(depths[:3]):sum(depths[:4])],
-#                             use_checkpoint=use_checkpoint)
-#         self.h_a2 = conv(N, N, kernel_size=3, stride=2)
-#         self.h_a3 = RSTB_BN(dim=N, input_resolution=(8, 8), depth=depths[4], num_heads=num_heads[4],
-#                             window_size=window_size // 2, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-#                             drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[sum(depths[:4]):sum(depths[:5])],
-#                             use_checkpoint=use_checkpoint)
-#         self.h_a4 = conv(N, N, kernel_size=3, stride=2)
-#
-#         depths = depths[::-1]
-#         num_heads = num_heads[::-1]
-#         self.h_s0 = deconv(N, N, kernel_size=3, stride=2)
-#         self.h_s1 = RSTB_BN(dim=N, input_resolution=(8, 8), depth=depths[0], num_heads=num_heads[0],
-#                             window_size=window_size // 2, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-#                             drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[sum(depths[:0]):sum(depths[:1])],
-#                             use_checkpoint=use_checkpoint)
-#         self.h_s2 = deconv(N, N, kernel_size=3, stride=2)
-#         self.h_s3 = RSTB_BN(dim=N, input_resolution=(16, 16), depth=depths[1], num_heads=num_heads[1],
-#                             window_size=window_size // 2, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-#                             drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[sum(depths[:1]):sum(depths[:2])],
-#                             use_checkpoint=use_checkpoint)
-#         self.h_s4 = conv(N, M * 2, kernel_size=3, stride=1)
-#
-#         self.g_s0 = deconv(M, N, kernel_size=3, stride=2)
-#         self.g_s1 = RSTB_BN(dim=N, input_resolution=(32, 32), depth=depths[2], num_heads=num_heads[2],
-#                             window_size=window_size, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-#                             drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[sum(depths[:2]):sum(depths[:3])],
-#                             use_checkpoint=use_checkpoint)
-#         self.g_s2 = deconv(N, N, kernel_size=3, stride=2)
-#         self.g_s3 = RSTB_BN(dim=N, input_resolution=(64, 64), depth=depths[3], num_heads=num_heads[3],
-#                             window_size=window_size, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-#                             drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[sum(depths[:3]):sum(depths[:4])],
-#                             use_checkpoint=use_checkpoint)
-#         self.g_s4 = deconv(N, N, kernel_size=3, stride=2)
-#         self.g_s5 = RSTB_BN(dim=N, input_resolution=(128, 128), depth=depths[4], num_heads=num_heads[4],
-#                             window_size=window_size, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-#                             drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[sum(depths[:4]):sum(depths[:5])],
-#                             use_checkpoint=use_checkpoint)
-#         self.g_s6 = deconv(N, 3, kernel_size=5, stride=2)
-#
-#         self.entropy_bottleneck = EntropyBottleneck(N)
-#         self.gaussian_conditional = GaussianConditional(None)
-#         self.context_prediction = CausalAttentionModule(M, M * 2)
-#         self.entropy_parameters = nn.Sequential(
-#             nn.Conv2d(M * 12 // 3, M * 10 // 3, 1), nn.GELU(),
-#             nn.Conv2d(M * 10 // 3, M * 8 // 3, 1), nn.GELU(),
-#             nn.Conv2d(M * 8 // 3, M * 6 // 3, 1))
-#         self.apply(self._init_weights)
-#
-#     def g_a(self, x, x_size=None):
-#         if x_size is None: x_size = x.shape[2:4]
-#         x = self.g_a0(x)
-#         x = self.g_a1(x, (x_size[0] // 2, x_size[1] // 2))
-#         x = self.g_a2(x)
-#         x = self.g_a3(x, (x_size[0] // 4, x_size[1] // 4))
-#         x = self.g_a4(x)
-#         x = self.g_a5(x, (x_size[0] // 8, x_size[1] // 8))
-#         x = self.g_a6(x)
-#         return x
-#
-#     def g_s(self, x, x_size=None):
-#         if x_size is None: x_size = (x.shape[2] * 16, x.shape[3] * 16)
-#         x = self.g_s0(x)
-#         x = self.g_s1(x, (x_size[0] // 8, x_size[1] // 8))
-#         x = self.g_s2(x)
-#         x = self.g_s3(x, (x_size[0] // 4, x_size[1] // 4))
-#         x = self.g_s4(x)
-#         x = self.g_s5(x, (x_size[0] // 2, x_size[1] // 2))
-#         x = self.g_s6(x)
-#         return x
-#
-#     def h_a(self, x, x_size=None):
-#         if x_size is None: x_size = (x.shape[2] * 16, x.shape[3] * 16)
-#         x = self.h_a0(x)
-#         x = self.h_a1(x, (x_size[0] // 16, x_size[1] // 16))
-#         x = self.h_a2(x)
-#         x = self.h_a3(x, (x_size[0] // 32, x_size[1] // 32))
-#         x = self.h_a4(x)
-#         return x
-#
-#     def h_s(self, x, x_size=None):
-#         if x_size is None: x_size = (x.shape[2] * 64, x.shape[3] * 64)
-#         x = self.h_s0(x)
-#         x = self.h_s1(x, (x_size[0] // 32, x_size[1] // 32))
-#         x = self.h_s2(x)
-#         x = self.h_s3(x, (x_size[0] // 16, x_size[1] // 16))
-#         x = self.h_s4(x)
-#         return x
-#
-#     def aux_loss(self):
-#         return sum(m.loss() for m in self.modules() if isinstance(m, EntropyBottleneck))
-#
-#     def _init_weights(self, m):
-#         if isinstance(m, nn.Linear):
-#             trunc_normal_(m.weight, std=.02)
-#             if isinstance(m, nn.Linear) and m.bias is not None:
-#                 nn.init.constant_(m.bias, 0)
-#         elif isinstance(m, nn.BatchNorm1d):
-#             nn.init.constant_(m.weight, 1.0)
-#             nn.init.constant_(m.bias, 0)
-#
-#     @torch.jit.ignore
-#     def no_weight_decay_keywords(self):
-#         return {'relative_position_bias_table'}
-#
-#     def forward(self, x):
-#         x_size = (x.shape[2], x.shape[3])
-#         y = self.g_a(x, x_size)
-#         z = self.h_a(y, x_size)
-#         z_hat, z_likelihoods = self.entropy_bottleneck(z)
-#         params = self.h_s(z_hat, x_size)
-#         y_hat = self.gaussian_conditional.quantize(y, "noise" if self.training else "dequantize")
-#         ctx_params = self.context_prediction(y_hat)
-#         gaussian_params = self.entropy_parameters(torch.cat((params, ctx_params), dim=1))
-#         scales_hat, means_hat = gaussian_params.chunk(2, 1)
-#         _, y_likelihoods = self.gaussian_conditional(y, scales_hat, means=means_hat)
-#         x_hat = self.g_s(y_hat, x_size)
-#
-#         # ✨✨✨【唯一修改處】✨✨✨
-#         # 在回傳的字典中，額外加入 'y' 這個鍵值對
-#         return {
-#             "x_hat": x_hat,
-#             "likelihoods": {"y": y_likelihoods, "z": z_likelihoods},
-#             "y": y
-#         }
-#
-#     def update(self, scale_table=None, force=False):
-#         if scale_table is None:
-#             scale_table = get_scale_table()
-#         self.gaussian_conditional.update_scale_table(scale_table, force=force)
-#         updated = any(m.update(force=force) for m in self.children() if isinstance(m, EntropyBottleneck))
-#         return updated
-#
-#     def load_state_dict(self, state_dict, strict=True):
-#         update_registered_buffers(self.entropy_bottleneck, "entropy_bottleneck",
-#                                   ["_quantized_cdf", "_offset", "_cdf_length"], state_dict)
-#         update_registered_buffers(self.gaussian_conditional, "gaussian_conditional",
-#                                   ["_quantized_cdf", "_offset", "_cdf_length", "scale_table"], state_dict)
-#         super().load_state_dict(state_dict, strict=strict)
-#
-#     @classmethod
-#     def from_state_dict(cls, state_dict):
-#         N = state_dict["g_a0.weight"].size(0)
-#         M = state_dict["g_a6.weight"].size(0)
-#         net = cls(N, M)
-#         net.load_state_dict(state_dict)
-#         return net
-#
-#     # ... compress 和 decompress 函式維持不變 ...
-#     def compress(self, x):
-#         y = self.g_a(x)
-#         z = self.h_a(y)
-#         z_strings = self.entropy_bottleneck.compress(z)
-#         z_hat = self.entropy_bottleneck.decompress(z_strings, z.size()[-2:])
-#         params = self.h_s(z_hat)
-#         s = 4
-#         kernel_size = 5
-#         padding = (kernel_size - 1) // 2
-#         y_height = z_hat.size(2) * s
-#         y_width = z_hat.size(3) * s
-#         y_hat = F.pad(y, (padding, padding, padding, padding))
-#         cdf = self.gaussian_conditional._quantized_cdf.tolist()
-#         cdf_lengths = self.gaussian_conditional._cdf_length.reshape(-1).int().tolist()
-#         offsets = self.gaussian_conditional._offset.reshape(-1).int().tolist()
-#         y_strings = []
-#         for i in range(y.size(0)):
-#             encoder = BufferedRansEncoder()
-#             symbols_list = []
-#             indexes_list = []
-#             for h in range(y_height):
-#                 for w in range(y_width):
-#                     y_crop = y_hat[i:i + 1, :, h:h + kernel_size, w:w + kernel_size]
-#                     ctx_p = self.context_prediction(y_crop)
-#                     p = params[i:i + 1, :, h:h + 1, w:w + 1]
-#                     gaussian_params = self.entropy_parameters(torch.cat((p, ctx_p[i:i + 1, :, 2:3, 2:3]), dim=1))
-#                     scales_hat, means_hat = gaussian_params.chunk(2, 1)
-#                     indexes = self.gaussian_conditional.build_indexes(scales_hat)
-#                     y_q = torch.round(y_crop - means_hat)
-#                     y_hat[i, :, h + padding, w + padding] = (y_q + means_hat)[i, :, padding, padding]
-#                     symbols_list.extend(y_q[i, :, padding, padding].int().tolist())
-#                     indexes_list.extend(indexes[i, :].squeeze().int().tolist())
-#             encoder.encode_with_indexes(symbols_list, indexes_list, cdf, cdf_lengths, offsets)
-#             string = encoder.flush()
-#             y_strings.append(string)
-#         return {"strings": [y_strings, z_strings], "shape": z.size()[-2:]}
-#
-#     def decompress(self, strings, shape):
-#         assert isinstance(strings, list) and len(strings) == 2
-#         z_hat = self.entropy_bottleneck.decompress(strings[1], shape)
-#         params = self.h_s(z_hat)
-#         s = 4
-#         kernel_size = 5
-#         padding = (kernel_size - 1) // 2
-#         y_height = z_hat.size(2) * s
-#         y_width = z_hat.size(3) * s
-#         y_hat = torch.zeros((z_hat.size(0), self.M, y_height + 2 * padding, y_width + 2 * padding), device=z_hat.device)
-#         decoder = RansDecoder()
-#         cdf = self.gaussian_conditional._quantized_cdf.tolist()
-#         cdf_lengths = self.gaussian_conditional._cdf_length.reshape(-1).int().tolist()
-#         offsets = self.gaussian_conditional._offset.reshape(-1).int().tolist()
-#         for i, y_string in enumerate(strings[0]):
-#             decoder.set_stream(y_string)
-#             for h in range(y_height):
-#                 for w in range(y_width):
-#                     y_crop = y_hat[i:i + 1, :, h:h + kernel_size, w:w + kernel_size]
-#                     ctx_p = self.context_prediction(y_crop)
-#                     p = params[i:i + 1, :, h:h + 1, w:w + 1]
-#                     gaussian_params = self.entropy_parameters(torch.cat((p, ctx_p[i:i + 1, :, 2:3, 2:3]), dim=1))
-#                     scales_hat, means_hat = gaussian_params.chunk(2, 1)
-#                     indexes = self.gaussian_conditional.build_indexes(scales_hat)
-#                     rv = decoder.decode_stream(indexes[i, :].squeeze().int().tolist(), cdf, cdf_lengths, offsets)
-#                     rv = torch.Tensor(rv).reshape(1, -1, 1, 1).to(z_hat.device)
-#                     rv = self.gaussian_conditional._dequantize(rv, means_hat)
-#                     y_hat[i, :, h + padding:h + padding + 1, w + padding:w + padding + 1] = rv
-#         y_hat = y_hat[:, :, padding:-padding, padding:-padding]
-#         x_hat = self.g_s(y_hat).clamp_(0, 1)
-#         return {"x_hat": x_hat}
-
 import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 from compressai.entropy_models import EntropyBottleneck, GaussianConditional
-from compressai.layers import RSTB, CausalAttentionModule
+from compressai.layers import RSTB, CausalAttentionModule, GDN
 from compressai.ans import BufferedRansEncoder, RansDecoder
 from timm.models.layers import trunc_normal_
 # [--- MODIFIED START ---]
@@ -516,6 +232,16 @@ class TIC(nn.Module):
         self.gaussian_conditional = GaussianConditional(None)
         self.context_prediction = CausalAttentionModule(M, M * 2)
 
+        # GDN layers for encoder (g_a)
+        self.gdn0 = GDN(N)
+        self.gdn1 = GDN(N)
+        self.gdn2 = GDN(N)
+
+        # IGDN layers for decoder (g_s)
+        self.igdn0 = GDN(N, inverse=True)
+        self.igdn1 = GDN(N, inverse=True)
+        self.igdn2 = GDN(N, inverse=True)
+
         self.entropy_parameters = nn.Sequential(
             nn.Conv2d(M * 12 // 3, M * 10 // 3, 1),
             nn.GELU(),
@@ -530,10 +256,13 @@ class TIC(nn.Module):
         if x_size is None:
             x_size = x.shape[2:4]
         x = self.g_a0(x)
+        x = self.gdn0(x)
         x = self.g_a1(x, (x_size[0] // 2, x_size[1] // 2))
         x = self.g_a2(x)
+        x = self.gdn1(x)
         x = self.g_a3(x, (x_size[0] // 4, x_size[1] // 4))
         x = self.g_a4(x)
+        x = self.gdn2(x)
         x = self.g_a5(x, (x_size[0] // 8, x_size[1] // 8))
         x = self.g_a6(x)
         return x
@@ -542,10 +271,13 @@ class TIC(nn.Module):
         if x_size is None:
             x_size = (x.shape[2] * 16, x.shape[3] * 16)
         x = self.g_s0(x)
+        x = self.igdn0(x)
         x = self.g_s1(x, (x_size[0] // 8, x_size[1] // 8))
         x = self.g_s2(x)
+        x = self.igdn1(x)
         x = self.g_s3(x, (x_size[0] // 4, x_size[1] // 4))
         x = self.g_s4(x)
+        x = self.igdn2(x)
         x = self.g_s5(x, (x_size[0] // 2, x_size[1] // 2))
         x = self.g_s6(x)
         return x
